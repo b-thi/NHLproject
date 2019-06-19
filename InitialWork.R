fc5cd3bb146d27844c16412349a5a93445604004
Nolan O'Reilly <oreilly.nolan@gmail.com>
2019-06-18 21:21:16 -0700

    commit

--- InitialWork.R
+++ InitialWork.R
@@ -1,3 +1,18 @@
-# Testing 123 
+library(tidyverse)
 
-# hello
+# Filter for games which are in the playoffs vs regular season
+playoffs <- game %>% 
+  select(game_id:home_goals) %>% 
+  filter(type == "P")
+
+regular_season <- game %>% 
+  select(game_id:home_goals) %>% 
+  filter(type == "R")
+
+# match the game id's in the plays data to separate the play by play into
+# playoffs and regular season
+playoffs_plays <- game_plays %>% 
+  filter(game_id %in% playoffs$game_id)
+
+regular_season_plays <- game_plays %>% 
+  filter(game_id %in% regular_season$game_id)
