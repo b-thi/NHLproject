2504be97ddfdf56112492d262ce7fbb098ee2820
Nolan O'Reilly <oreilly.nolan@gmail.com>
2019-06-18 21:25:12 -0700

    commit

--- InitialWork.R
+++ InitialWork.R
@@ -12,7 +12,10 @@ regular_season <- game %>%
 # match the game id's in the plays data to separate the play by play into
 # playoffs and regular season
 playoffs_plays <- game_plays %>% 
-  filter(game_id %in% playoffs$game_id)
+  filter(game_id %in% playoffs$game_id) %>% 
+  mutate(goal_diff = goals_home - goals_away)
 
 regular_season_plays <- game_plays %>% 
-  filter(game_id %in% regular_season$game_id)
+  filter(game_id %in% regular_season$game_id) %>% 
+  mutate(goal_diff = goals_home - goals_away)
+
